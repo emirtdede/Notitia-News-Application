927eee529669e2a21f6c754301c491a479519664
Emir Dede <59409331+emirtdede@users.noreply.github.com>
2024-10-10 17:52:16 +0300

    Update

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBQ-TTEfqxZg0wAERVShv6pCQQQFZJnYAA",
+  apiKey: "your_api_key",
   authDomain: "notitiaapp-rn.firebaseapp.com",
   projectId: "notitiaapp-rn",
   storageBucket: "notitiaapp-rn.appspot.com",
   messagingSenderId: "1087137037590",
-  appId: "1:1087137037590:web:e3590a1d7c7efe1f26a437",
-  measurementId: "G-Q0NL52C614"
+  appId: "your_app_id",
+  measurementId: "your_measurement_id"
 };
 
 const app = initializeApp(firebaseConfig);
